8528b6f0b597b29660907d66aeba14861d34e5be
HugoLorent <77060035+HugoLorent@users.noreply.github.com>
2022-05-03 11:07:30 +0200

    add move for cube
    Signed-off-by: HugoLorent <77060035+HugoLorent@users.noreply.github.com>

---
+++ Assets/_Project/Scripts/cubeMove.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeMove : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKey(KeyCode.UpArrow))
+    {
+      GetComponent<Rigidbody>().MovePosition(transform.position + Vector3.up * 0.2f);
+    }
+
+    if (Input.Getkey(KeyCode.DownArrow))
+    {
+      GetComponent<Rigidbody>().MovePosition(transform.position + Vector3.down * 0.2f);
+    }
+
+    if (Input.Getkey(KeyCode.LeftArrow))
+    {
+      GetComponent<Rigidbody>().MovePosition(transform.position + Vector3.left * 0.2f);
+    }
+
+    if (Input.Getkey(KeyCode.RightArrow))
+    {
+      GetComponent<Rigidbody>().MovePosition(transform.position + Vector3.right * 0.2f);
+    }
+  }
+}
